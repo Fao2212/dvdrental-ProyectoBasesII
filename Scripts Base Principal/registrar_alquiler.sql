1e8dd8a3411e721435ef4b608ab2c06305829d0c
Fernando Alvarez <ferolsen@hotmail.com>
2021-05-23 22:27:57 -0600

    Cambio a registro de alquiler con su validacion respectiva

--- Scripts Base Principal/registrar_alquiler.sql
+++ Scripts Base Principal/registrar_alquiler.sql
@@ -5,13 +5,14 @@ CREATE OR REPLACE PROCEDURE registrar_alquiler(
 )
 AS
 $$
-BEGIN--Se deja la fecha de retorno nula? Se usa usa un trigger?
-	INSERT INTO rental(rental_date,inventory_id,customer_id,staff_id,last_update)
-	VALUES (CURRENT_TIMESTAMP,_inventory_id,_customer_id,_staff_id,CURRENT_TIMESTAMP);
+BEGIN
+	IF inventory_in_stock(_inventory_id) THEN
+		INSERT INTO rental(rental_date,inventory_id,customer_id,staff_id,last_update)
+		VALUES (CURRENT_TIMESTAMP,_inventory_id,_customer_id,_staff_id,CURRENT_TIMESTAMP);
+	END IF;
 END;
 $$ LANGUAGE plpgsql
 SECURITY DEFINER
-
 GRANT EXECUTE ON PROCEDURE registrar_alquiler TO EMP;
 
 call registrar_alquiler(2,2::smallint,2::smallint);
